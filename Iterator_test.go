08d33058927c22b2dce9130d2fffe0e6b8f103bf
asamaayako <2433047519@qq.com>
2024-08-16 10:53:20 +0800

    1.23 跟新 修改函数常用函数

--- Iterator_test.go
+++ Iterator_test.go
@@ -2,42 +2,17 @@ package fp
 
 import (
 	"fmt"
-	"os"
-	"strings"
+	"slices"
 	"testing"
 )
 
 func TestIterrator(t *testing.T) {
-	// 使用 NewIntRange 函数生成迭代器
-	s := "dagsdaga"
-	person := &Person{Name: "Bob", Age: 25}
-	err := printPersonWithoutEscape(person)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error printing person:", err)
-	}
 
-	println(&s)
-	println(&person)
-}
+	si := slices.Values(slices.Repeat([]int{1, 2, 3, 4}, 3))
 
-type Person struct {
-	Name string
-	Age  int
-}
+	s1, s2 := Split(si)
 
-func (m Person) String() string {
-	return fmt.Sprintf("MyStruct{Name: %s, Age: %d}", m.Name, m.Age)
-}
-func (m Person) GoString() string {
-	return fmt.Sprintf("MyStruct{Name: %#v, Age: %#v}", m.Name, m.Age)
-}
-func printPersonWithoutEscape(p *Person) error {
-	var output strings.Builder
-	output.WriteString(p.String())
-	output.Write([]byte("\n"))
-	output.WriteString(p.GoString())
-	output.Write([]byte("\n"))
-	// 将内容写入标准输出，避免直接使用fmt.Println可能引起的逃逸。
-	_, err := os.Stdout.WriteString(output.String())
-	return err
+	fmt.Println(slices.Collect(Take[int](2)(s1)))
+	fmt.Println(slices.Collect(Drop[int](2)(s2)))
+	fmt.Println(slices.Collect(Drop[int](0)(Compose[int, int, int](Map(func(i int) int { return i / 2 }), Filter(func(i int) bool { return i%2 == 0 }))(s1))))
 }
--- fpfunc.go
+++ fpfunc.go
@@ -7,83 +7,112 @@
 
 package fp
 
-// 右折叠
-// Foldr:: ((a -> b -> a) -> a) -> [b] -> a
-func Foldr[a, b any](f func(a, b) a, acc a) func([]b) a {
-	return func(bs []b) a {
-		if len(bs) == 0 {
-			return acc
+import "iter"
+
+// Fold:: ((a -> b -> a) -> a) -> [b] -> a
+func Fold[a, b any](f func(a, b) a, acc a) func(iter.Seq[b]) a {
+	return func(bs iter.Seq[b]) a {
+		for bv := range bs {
+			acc = f(acc, bv)
 		}
-		return f(Foldr(f, acc)(bs[1:]), bs[0])
+		return acc
 	}
 }
 
-// 左折叠
-// FoldL:: ((a -> b -> a) -> a) -> [b] -> b
-func FoldL[a, b any](f func(a, b) a, acc a) func([]b) a {
-	return func(bs []b) a {
-		if len(bs) == 0 {
-			return acc
+func Take[a any](n int) func(seq iter.Seq[a]) iter.Seq[a] {
+	return func(seq iter.Seq[a]) iter.Seq[a] {
+		var count int
+		return func(yield func(a) bool) {
+			seq(func(v a) bool {
+				count++
+				if count <= n {
+					return yield(v)
+				}
+				return false
+			})
 		}
-		return FoldL(f, f(acc, bs[0]))(bs[1:])
 	}
+}
+
+func Drop[a any](n int) func(seq iter.Seq[a]) iter.Seq[a] {
+	return func(seq iter.Seq[a]) iter.Seq[a] {
+		return func(yield func(a) bool) {
+			var count int
+			seq(func(v a) bool {
+				count++
+				if count <= n {
+					return true
+				}
+				return yield(v)
+			})
+		}
+	}
+}
+
+func Reduce[a, b any](f func(a, b) a) func(iter.Seq[b]) a {
+	var av a
+	return Fold(f, av)
+}
+
+func Zip[Fir, Sec any](seq2 iter.Seq2[Fir, Sec]) iter.Seq[Pair[Fir, Sec]] {
+	return func(yield func(Pair[Fir, Sec]) bool) {
+		seq2(func(fir Fir, sec Sec) bool {
+			return yield(Pair[Fir, Sec]{fir, sec})
+		})
+	}
+}
+
+// UnZip :: [Pair<a,b>] -> [a] -> [b]
+func UnZip[Fir, Sec any](seq iter.Seq[Pair[Fir, Sec]]) iter.Seq2[Fir, Sec] {
+	return func(yield func(Fir, Sec) bool) {
+		seq(func(p Pair[Fir, Sec]) bool {
+			return yield(p.First, p.Second)
+		})
+	}
+}
+
+// Split :: [a] -> ([a],[a])
+func Split[a any](seq iter.Seq[a]) (iter.Seq[a], iter.Seq[a]) {
+	seq1 := seq // iter.Seq 是只读的因此简单复制不会出现问题
+	return seq1, seq
 }
 
 // Map :: (a -> b) -> [a] -> [b]
-func Map[a, b any](f func(a) b) func([]a) []b {
-	return func(as []a) []b {
-		return FoldL(func(b1 []b, a1 a) []b { return append(b1, f(a1)) }, make([]b, 0, len(as)))(as)
+func Map[a, b any](f func(a) b) func(iter.Seq[a]) iter.Seq[b] {
+	return func(as iter.Seq[a]) iter.Seq[b] {
+		return func(yield func(b) bool) {
+			as(func(a a) bool {
+				return yield(f(a))
+			})
+		}
 	}
 }
 
 // Filter :: (a -> Bool) -> [a] -> [a]
-func Filter[a any](p func(a) bool) func([]a) []a {
-	return func(as []a) []a {
-		return FoldL(func(a2 []a, a1 a) []a {
-			if p(a1) {
-				return append(a2, a1)
-			} else {
-				return a2
-			}
-		}, make([]a, 0, len(as)))(as)
-	}
-}
-
-// Compose :: f (function [t]->[t]) t =>[([t]->[t])]->[t]->[t]
-func Compose[T any](fnList ...func(...T) []T) func(...T) []T {
-	return func(s ...T) []T {
-		f := fnList[0]
-		nextFnList := fnList[1:]
-
-		if len(fnList) == 1 {
-			return f(s...)
+func Filter[a any](p func(a) bool) func(iter.Seq[a]) iter.Seq[a] {
+	return func(as iter.Seq[a]) iter.Seq[a] {
+		return func(yield func(a) bool) {
+			as(func(av a) bool {
+				if p(av) {
+					yield(av)
+				}
+				return true
+			})
+			return
 		}
-
-		return f(Compose(nextFnList...)(s...)...)
 	}
 }
 
-// ComposeInterface Compose the functions from right to left (Math: f(g(x)) Compose: Compose(f, g)(x))
-func ComposeInterface(fnList ...func(...interface{}) []interface{}) func(...interface{}) []interface{} {
-	return Compose(fnList...)
+// Compose :: ([b]->[c])->([a]->[b])->[t]->[t]
+func Compose[a, b, c any](f func(iter.Seq[b]) iter.Seq[c], g func(iter.Seq[a]) iter.Seq[b]) func(iter.Seq[a]) iter.Seq[c] {
+	return func(as iter.Seq[a]) iter.Seq[c] {
+		return f(g(as))
+	}
 }
 
 // Pipe Pipe the functions from left to right
-func Pipe[T any](fnList ...func(...T) []T) func(...T) []T {
-	return func(s ...T) []T {
-		lastIndex := len(fnList) - 1
-		f := fnList[lastIndex]
-		nextFnList := fnList[:lastIndex]
-
-		if len(fnList) == 1 {
-			return f(s...)
-		}
-
-		return f(Pipe(nextFnList...)(s...)...)
+func Pipe[a, b, c any](f func(iter.Seq[a]) iter.Seq[b], g func(iter.Seq[b]) iter.Seq[c]) func(iter.Seq[a]) iter.Seq[c] {
+	return func(as iter.Seq[a]) iter.Seq[c] {
+		return g(f(as))
 	}
-}
-
-// PipeInterface Pipe the functions from left to right
-func PipeInterface(fnList ...func(...interface{}) []interface{}) func(...interface{}) []interface{} {
-	return Pipe(fnList...)
 }
