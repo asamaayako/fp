be20fe23973b8259f79865d24eee1233b64cfe15
asamaayako <2433047519@qq.com>
2025-08-28 05:00:26 +0800

    add some new func

--- fpfunc.go
+++ fpfunc.go
@@ -4,14 +4,17 @@
  * Author: asamaayako
  * Date: 2024/4/9
  * Time: 上午9:07
- * Describe:
+ * Describe: 配合slices库食用
  */
 package fp
 
-import "iter"
+import (
+	"iter"
+	"slices"
+)
 
 // Fold:: ((a -> b -> a) -> a) -> [b] -> a
-func Fold[a, b any](f func(a, b) a, acc a) func(iter.Seq[b]) a {
+func Fold[a, b any](acc a, f func(a, b) a) func(iter.Seq[b]) a {
 	return func(bs iter.Seq[b]) a {
 		for bv := range bs {
 			acc = f(acc, bv)
@@ -28,8 +31,8 @@ func Head[a any]() func(iter.Seq[a]) iter.Seq[a] {
 // Take :: Int -> [a] -> [a]
 func Take[a any](n int) func(iter.Seq[a]) iter.Seq[a] {
 	return func(seq iter.Seq[a]) iter.Seq[a] {
-		var count int
 		return func(yield func(a) bool) {
+			var count int
 			seq(func(v a) bool {
 				count++
 				if count <= n {
@@ -39,6 +42,11 @@ func Take[a any](n int) func(iter.Seq[a]) iter.Seq[a] {
 			})
 		}
 	}
+}
+func Len[T any](seq iter.Seq[T]) int {
+	return Fold(0, func(sum int, _ T) int {
+		return sum + 1
+	})(seq)
 }
 
 // Tail :: [a] -> [a]
@@ -65,15 +73,28 @@ func Drop[a any](n int) func(seq iter.Seq[a]) iter.Seq[a] {
 // Reduce :: (a -> b -> a) -> a -> [b] -> a
 func Reduce[a, b any](f func(a, b) a) func(iter.Seq[b]) a {
 	var av a
-	return Fold(f, av)
+	return Fold(av, f)
 }
 
+// 需要同时从两个序列中获取元素
 // Zip :: [a] -> [b] -> [Pair<a,b>]
-func Zip[Fir, Sec any](seq2 iter.Seq2[Fir, Sec]) iter.Seq[Pair[Fir, Sec]] {
-	return func(yield func(Pair[Fir, Sec]) bool) {
-		seq2(func(fir Fir, sec Sec) bool {
-			return yield(Pair[Fir, Sec]{fir, sec})
-		})
+func Zip[T, U any](seq1 iter.Seq[T], seq2 iter.Seq[U]) iter.Seq[Pair[T, U]] {
+	return func(yield func(Pair[T, U]) bool) {
+		next1, stop1 := iter.Pull(seq1)
+		defer stop1()
+		next2, stop2 := iter.Pull(seq2)
+		defer stop2()
+
+		for {
+			v1, ok1 := next1()
+			v2, ok2 := next2()
+			if !ok1 || !ok2 {
+				return
+			}
+			if !yield(Pair[T, U]{v1, v2}) {
+				return
+			}
+		}
 	}
 }
 
@@ -86,10 +107,119 @@ func UnZip[Fir, Sec any](seq iter.Seq[Pair[Fir, Sec]]) iter.Seq2[Fir, Sec] {
 	}
 }
 
+func UnZipSeq2[Fir, Sec any](seq2 iter.Seq2[Fir, Sec]) (iter.Seq[Fir], iter.Seq[Sec]) {
+	var firsts []Fir
+	var seconds []Sec
+
+	// 提取所有元素到缓存中
+	seq2(func(f Fir, s Sec) bool {
+		firsts = append(firsts, f)
+		seconds = append(seconds, s)
+		return true
+	})
+
+	return slices.Values(firsts), slices.Values(seconds)
+}
+
 // Split :: [a] -> ([a],[a])
 func Split[a any](seq iter.Seq[a]) (iter.Seq[a], iter.Seq[a]) {
-	seq1 := seq // iter.Seq 是只读的因此简单复制不会出现问题
+	seq1 := seq //流可以重复使用 注意作用域即可
 	return seq1, seq
+}
+
+// 将序列分块处理
+// Chunk:: int->[a]->[[a]]
+func Chunk[T any](size int) func(seq iter.Seq[T]) iter.Seq[[]T] {
+	return func(seq iter.Seq[T]) iter.Seq[[]T] {
+		return func(yield func([]T) bool) {
+			var chunk = make([]T, 0, size)
+			seq(func(i T) bool {
+				chunk = append(chunk, i)
+				if len(chunk) == size {
+					res := yield(chunk)
+					chunk = make([]T, 0)
+					return res
+				}
+				return true
+			})
+			if len(chunk) > 0 {
+				yield(chunk)
+			}
+		}
+	}
+
+}
+
+// 创建滑动窗口
+// SlidingWindow:: int -> [T]->[[T]]
+// used read only
+func SlidingWindow[T any](n int) func(iter.Seq[T]) iter.Seq[[]T] {
+	return func(seq iter.Seq[T]) iter.Seq[[]T] {
+		return func(yield func([]T) bool) {
+			var window []T = make([]T, 0, n)
+
+			seq(func(item T) bool {
+				window = append(window, item)
+				if len(window) > n {
+					window = window[1:]
+				}
+				//积累窗口到达n时 产出一个窗口
+				if len(window) == n {
+					windowCopy := make([]T, len(window))
+					copy(windowCopy, window)
+					if !yield(windowCopy) {
+						return false
+					}
+				}
+				return true
+			})
+		}
+	}
+}
+
+// 当满足某个条件时才继续迭代
+// TakeWhile:: (T->bool)->[T]->[T]
+func TakeWhile[T any](predicate func(T) bool) func(iter.Seq[T]) iter.Seq[T] {
+	return func(seq iter.Seq[T]) iter.Seq[T] {
+		return func(yield func(T) bool) {
+			seq(func(item T) bool {
+				if !predicate(item) {
+					return false
+				}
+				return yield(item)
+			})
+		}
+	}
+}
+
+// 处理相邻元素对
+// AdjacentPairs:: [T]->[Pair(T,T)]
+func AdjacentPairs[T any](seq iter.Seq[T]) iter.Seq[[2]T] {
+	return func(yield func([2]T) bool) {
+		var prev T
+		var hasPrev bool
+		seq(func(item T) bool {
+			if !hasPrev {
+				prev = item
+				hasPrev = true
+				return true
+			}
+			if !yield([2]T{prev, item}) {
+				return false
+			}
+			prev = item
+			return true
+		})
+	}
+}
+
+func Pairs[T any](seq iter.Seq[T]) iter.Seq[[2]T] {
+	return func(yield func([2]T) bool) {
+		chunks := Chunk[T](2)(seq)
+		chunks(func(item []T) bool {
+			return yield([2]T{item[0], item[1]})
+		})
+	}
 }
 
 // Map :: (a -> b) -> [a] -> [b]
